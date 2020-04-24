295c923015eba9becd19417b4ce3ebbdc1985a11
VormJS <mjsnot@gmail.com>
2020-04-24 12:12:54 +0300

    Add Basic Login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,3 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-
-  def hello
-    render html: "Greetings from RubyGram!"
-  end
+  include SessionsHelper
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,7 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
-require "minitest/reporters"
+require 'minitest/reporters'
 Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
@@ -11,5 +11,8 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  # Returns true if a test user is logged in.
+  def logged_in?
+    !session[:user_id].nil?
+  end
 end
